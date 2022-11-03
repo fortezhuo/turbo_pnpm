3589005456e5ad0828cf88955c9b0bb3bbc9bb45
Forte Zhuo <hedikasmanto@gmail.com>
2022-11-03 11:51:14 +0700

    First Commit

---
+++ packages/ui/Button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Text } from "ui/Text";
+export const Button = () => {
+  return (
+    <>
+      <button>Boop</button>
+      <Text>Hello World</Text>
+    </>
+  );
+};
